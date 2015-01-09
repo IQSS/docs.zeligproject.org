5b8474c2737724390f2fc921bc1eecca70a88357
tercer <jhonaker@iq.harvard.edu>
2015-01-09 10:12:43 -0500

    adding sphinx extension

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/mast
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 # extensions = ['sphinx.ext.mathjax']
-extensions = ['sphinx.ext.pngmath']
+extensions = ['sphinx.ext.pngmath','sphinxcontrib.programoutput']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
