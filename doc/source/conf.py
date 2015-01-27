ccb42869e88e5214d841a6cf89b253f2e5f0a58f
cchoirat <cchoirat@gmail.com>
2015-01-27 13:51:55 -0500

    Retrieve tree from zeligproject.org

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,15 +16,15 @@ sys.path.insert(0, os.path.abspath('../../'))
 import sphinx_bootstrap_theme
 
 # @cchoirat: download model tree and navbar files from zeligproject.org
-# import urllib
-# urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/zelig5models.json",
-#                    "_static/zelig5models.json")
-# urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/zelig5choicemodels.json",
-#                    "_static/zelig5choicemodels.json")
-# urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/modelstree.html",
-#                    "_static/modelstree.html")
-# urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/zelignav.html",
-#                    "zelignav.html")
+import urllib
+urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/zelig5models.json",
+                   "_static/zelig5models.json")
+urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/zelig5choicemodels.json",
+                   "_static/zelig5choicemodels.json")
+urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/_static/modelstree.html",
+                   "_static/modelstree.html")
+urllib.urlretrieve("https://raw.githubusercontent.com/IQSS/zeligproject.org/master/Sphinx/source/zelignav.html",
+                   "zelignav.html")
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
